db7501322cfe0ca6f0bc9e07e34210ea598ead8b
rafaelgodinho <rafaelgodinho@live.com>
2017-04-28 12:30:24 -0700

    Delete debugListResourceTag.sh

--- debugListResourceTag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-subscriptionId="<TODO>"
-tenantId="<TODO>"
-clientId="<TODO>"
-clientSecret="<TODO>"
-resourceGroupName="<TODO>"
-
-./listResourceTag.sh -i $subscriptionId -t $tenantId -c $clientId -s $clientSecret -g $resourceGroupName
